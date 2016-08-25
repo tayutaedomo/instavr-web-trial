f205bb4790255e1d4cef45fb62c01ef79ecd15e8
tayutaedomo <tayutaedomo@gmail.com>
2016-08-25 12:28:57 +0900

    Add template logic

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
+var title = 'InstaVR Web Embed Trial';
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'InstaVR Web Embed Trial' });
+  res.render('index', { title: title });
 });
 
 router.get('/web_embed', function(req, res, next) {
-  res.render('web_embed');
+  res.render('web_embed', { title : 'Web Embed | ' + title });
 });
 
 router.get('/web_embed_2', function(req, res, next) {
-  res.render('web_embed_2');
+  res.render('web_embed_2', { title : 'Web Embed 2 | ' + title });
 });
 
 router.get('/web_embed_2-1', function(req, res, next) {
-  res.render('web_embed_2-1');
+  res.render('web_embed_2-1', { title : 'Web Embed 2-1 | ' + title });
 });
 
 router.get('/web_embed_2-2', function(req, res, next) {
-  res.render('web_embed_2-2');
+  res.render('web_embed_2-2', { title : 'Web Embed 2-2 | ' + title });
 });
 
 router.get('/web_embed_3', function(req, res, next) {
-  res.render('web_embed_3');
+  res.render('web_embed_3', { title : 'Web Embed 3 | ' + title });
 });
 
 router.get('/web_embed_4', function(req, res, next) {
-  res.render('web_embed_4');
+  res.render('web_embed_4', { title : 'Web Embed 4 | ' + title });
 });
 
 router.get('/web_embed_5', function(req, res, next) {
-  res.render('web_embed_5');
+  res.render('web_embed_5', { title : 'Web Embed 5 | ' + title });
 });
 
 
