527db504be1bbd6b51942257ae3a9d60c860baeb
tayutaedomo <tayutaedomo@gmail.com>
2016-09-05 19:31:29 +0900

    Add cors middleware

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors')
 
 var routes = require('./routes/index');
 
@@ -21,6 +22,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
+//app.use('/public/resources', cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
