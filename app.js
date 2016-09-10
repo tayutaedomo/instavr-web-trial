2c2d4c32131e1484681940ba88e353e4cd21ac33
tayutaedomo <tayutaedomo@gmail.com>
2016-09-10 14:53:26 +0900

    Tweak app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var cors = require('cors')
+var cors = require('cors');
 
 var routes = require('./routes/index');
 
